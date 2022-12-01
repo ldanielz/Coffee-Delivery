267b6d3f126c930dab369e50118e561983eed42f
Lizandro Daniel <ldaniel.eng@gmail.com>
2022-12-01 15:34:28 -0300

    Corrigindo bugs

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
+export default defineConfig(({ command, mode }) => {
+  if (command === 'build') {
+    return {
+      base: '/Coffee-Delivery/',
+      plugins: [react()],
+    }
+  } else {
+    return {
+      plugins: [react()],
+    }
+  }
 })
