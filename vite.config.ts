1224d1fa6f039c139f2f2a50ac1bc22a529cdd28
Lizandro Daniel <ldaniel.eng@gmail.com>
2022-12-01 15:40:58 -0300

    Mudança na config do Vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  if (command === 'build') {
-    return {
-      base: '/Coffee-Delivery/',
-      plugins: [react()],
-    }
-  } else {
-    return {
-      plugins: [react()],
-    }
-  }
+export default defineConfig({
+  plugins: [react()],
 })
